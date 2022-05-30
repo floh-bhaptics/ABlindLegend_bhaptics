8615ea55dc721fce8f52910f89e0d048f566f606
floh-bhaptics <floh@fahrenbergers.de>
2022-05-30 20:35:12 +0200

    Projektdateien hinzufügen.

---
+++ ABlindLegend_bhaptics.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MelonLoader;
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+namespace ABlindLegend_bhaptics
+{
+    public class ABlindLegend_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        [HarmonyPatch(typeof(Hero), "ReceiveHit", new Type[] { typeof(EnemyStats), typeof(bool), typeof(bool)})]
+        public class bhaptics_PlayerReceivesHit
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Hero __instance, EnemyStats other)
+            {
+                tactsuitVr.LOG("Attack strength" + other.strength.ToString());
+            }
+        }
+
+        [HarmonyPatch(typeof(HeroStats), "RemoveLife", new Type[] {  })]
+        public class bhaptics_PlayerLives
+        {
+            [HarmonyPostfix]
+            public static void Postfix(HeroStats __instance)
+            {
+                int totalLives = __instance.lives + __instance.boughtLives;
+                if (totalLives == 1) tactsuitVr.StartHeartBeat();
+                else tactsuitVr.StopHeartBeat();
+            }
+        }
+
+        [HarmonyPatch(typeof(HurtArea), "Enter", new Type[] { typeof(UnityEngine.Collider) })]
+        public class bhaptics_PlayerHurtArea
+        {
+            [HarmonyPostfix]
+            public static void Postfix(HurtArea __instance)
+            {
+                tactsuitVr.LOG("HurtArea: " + __instance.areaName);
+            }
+        }
+        /*
+        [HarmonyPatch(typeof(Combat), "Update", new Type[] { })]
+        public class bhaptics_PlayerCombat
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Combat __instance)
+            {
+                if (__instance.input.justDid(InputActionName.PROTECT) != null)
+                    tactsuitVr.LOG("HurtArea: " + __instance.areaName);
+            }
+        }
+        */
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("ABlindLegend_bhaptics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ABlindLegend_bhaptics")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+[assembly: MelonInfo(typeof(ABlindLegend_bhaptics.ABlindLegend_bhaptics), "ABlindLegend_bhaptics", "1.0.0", "Florian Fahrenberger")]
+[assembly: MelonGame("Dowino", "A Blind Legend")]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("7b7370de-c702-4152-9dda-e28c2cda0c0c")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
